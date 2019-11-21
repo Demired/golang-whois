57aa9618ed4d76c75daf265a2abd805c4c513d1e
zhangyuan <nevermakeyoucrytoli@gmail.com>
2019-11-21 16:24:00 +0800

    add ParseExpiryDate func

--- extra.go
+++ extra.go
@@ -15,6 +15,7 @@ package whois
 import (
 	"regexp"
 	"strings"
+	"time"
 )
 
 func parser(re *regexp.Regexp, group int, data string) (result []string) {
@@ -34,6 +35,7 @@ func parser(re *regexp.Regexp, group int, data string) (result []string) {
 	return
 }
 
+//ParseNameServers func
 //Parse uniq name servers from whois
 func ParseNameServers(whois string) []string {
 
@@ -41,11 +43,31 @@ func ParseNameServers(whois string) []string {
 
 }
 
+//ParseDomainStatus func
 //Parse uniq domain status(codes) from whois
 func ParseDomainStatus(whois string) []string {
 
 	return parser(regexp.MustCompile(`(?i)(Domain )?Status:\s+(.*?)(\s|$)`), 2, whois)
 
+}
+
+//ParseExpiryDate func
+func ParseExpiryDate(whois string) time.Time {
+	expiryDates := parser(regexp.MustCompile(`(?i)(Registry Expiry Date|Expiration Time|paid-till):[ ]+(\d+\-\d+\-\d+[a-zA-Z ]?\d+:\d+:\d+[a-zA-Z ]?)`), 2, whois)
+	for _, v := range expiryDates {
+		var layout = ""
+		s := strings.ToUpper(v)
+		if strings.Contains(s, "T") && strings.Contains(s, "Z") {
+			layout = time.RFC3339
+		} else {
+			layout = "2006-01-02 15:04:05"
+		}
+		t, e := time.Parse(layout, s)
+		if e == nil {
+			return t.In(time.Local)
+		}
+	}
+	return time.Time{}
 }
 
 func appendIfMissing(slice []string, i string) []string {
